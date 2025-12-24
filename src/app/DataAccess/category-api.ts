b2150712b203d73eabcdd4d773ad743e7c403e34
mustafa.bayrak <mustafa.bayrak@KDK-302-YZ-PC20>
2025-12-24 14:05:31 +0300

    CRUD

---
+++ src/app/DataAccess/category-api.ts
@@ -0,0 +1,35 @@
+import { Injectable,inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { lastValueFrom } from "rxjs";
+import { API_Config } from "../api.config";
+import { CategoryResponseModel } from "../Models/Categories/CategoryResponseModel";
+import { CreateCategoryRequestModel } from "../Models/Categories/CreateCategoryRequestModel";
+import { UpdateCategoryRequestModel} from "../Models/Categories/UpdateCategoryRequestModel";
+
+@Injectable ( {providedIn:'root'})
+
+export class CategoryApi{
+    private http = inject(HttpClient);
+    private readonly url = `${API_Config.baseUrl}/${API_Config.endpoints.category}`;
+
+    async getAll():Promise<CategoryResponseModel[]>{
+        return await lastValueFrom(this.http.get<CategoryResponseModel[]>(this.url));
+    }
+
+    async create(body:CreateCategoryRequestModel):Promise<string>{
+        return await lastValueFrom(this.http.post(this.url,body,{
+            responseType:'text'
+        }));
+    }
+
+    async update(body:UpdateCategoryRequestModel):Promise<string>{
+        return await lastValueFrom(this.http.put(this.url,body,{
+            responseType:'text'
+        }));
+    }
+
+    async deleteById(id:number):Promise<string>{
+        return await lastValueFrom(this.http.delete(this.url,{body:id,responseType:'text'}));
+    }
+}
+
